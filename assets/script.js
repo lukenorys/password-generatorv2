181ea194a252b07d2bc38b04247df989d7a93fd6
lukenorys <lukenorys@ymail.com>
2021-01-24 18:37:23 -0600

    add pseudocode and initial window.prompt

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-// Assignment code here
+// declare functions needed upperCase / lowerCase / numbers / specChar needed ( " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") special characters.
+const pwLength = window.prompt("Type a number between 8 & 128 for password length. Example: 17")
+while (pwLength < 8 || howLong > 128) {
+    pwLength = window.prompt("Your selection must be a number between 8 and 128. Please type in a number value.");
+  }
+
+  pwLength = parseInt(pwLength);
+
+// add window.prompts for criteria. create validation to ensure at least one character type was selected
 
 
 // Get references to the #generate element
