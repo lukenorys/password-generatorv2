a727a7394d5afddc804e1f971845ab3131c0995f
lukenorys <lukenorys@ymail.com>
2021-01-24 18:55:28 -0600

    add validation for user field. ! not indicator from 3.5.7 used

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,21 @@ while (pwLength < 8 || howLong > 128) {
     pwLength = parseInt(pwLength);
   };
 
-//declare variables with validation that AT LEAST ONE field is selected
+//declare variables with validation that AT LEAST ONE field is selected (specChara needed ( " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") special characters.)
 let upperCase = confirm("Would you like to include uppercase letters?");
 let lowerCase = confirm("How about lowercase letters?");
 let numbChara = confirm("Let's add some numbers in there, yes?");
 let specChara = confirm("How about some special characters to top it off?");
 
-// declare functions / variables needed upperCase / lowerCase / numbers / specChar needed ( " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") special characters.
+// if user picks none, the not indicator, !, will trigger this response
+
+while(!(upperCase || lowerCase || numbChara || specChara)) {
+  window.alert("You must select at least one of the following when prompted.");
+    upperCase = confirm("Would you like to include uppercase letters?");
+    lowerCase = confirm("How about lowercase letters?");
+    numbChara = confirm("Let's add some numbers in there, yes?");
+    specChara = confirm("How about some special characters to top it off?");
+};
 
 
 // add window.prompts for criteria. create validation to ensure at least one character type was selected
