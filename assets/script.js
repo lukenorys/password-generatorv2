a4b0f86729a6da3b777fc40ac6aae089d51c1af3
lukenorys <lukenorys@ymail.com>
2021-01-24 18:49:07 -0600

    redirect links in HTML & start variables in JS.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
-// declare functions needed upperCase / lowerCase / numbers / specChar needed ( " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") special characters.
-const pwLength = window.prompt("Type a number between 8 & 128 for password length. Example: 17")
+// declare password length with validation for correct number range
+let pwLength = window.prompt("Type a number between 8 & 128 for password length. Example: 17")
 while (pwLength < 8 || howLong > 128) {
     pwLength = window.prompt("Your selection must be a number between 8 and 128. Please type in a number value.");
-  }
+    pwLength = parseInt(pwLength);
+  };
 
-  pwLength = parseInt(pwLength);
+//declare variables with validation that AT LEAST ONE field is selected
+let upperCase = confirm("Would you like to include uppercase letters?");
+let lowerCase = confirm("How about lowercase letters?");
+let numbChara = confirm("Let's add some numbers in there, yes?");
+let specChara = confirm("How about some special characters to top it off?");
+
+// declare functions / variables needed upperCase / lowerCase / numbers / specChar needed ( " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") special characters.
+
 
 // add window.prompts for criteria. create validation to ensure at least one character type was selected
 
